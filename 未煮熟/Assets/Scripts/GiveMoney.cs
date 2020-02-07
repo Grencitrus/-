2a37bcc5ff23a28270758d87cb40aaef29a72c8a
Grencitrus <60340957+Grencitrus@users.noreply.github.com>
2020-02-07 10:32:18 +0100

    costumer more than half done

---
+++ 未煮熟/Assets/Scripts/GiveMoney.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveMoney : MonoBehaviour
+{
+    public static float myMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CostumerMoney()
+    {
+        float tempMoney;
+        tempMoney = 25 + (75 * 0.01f);
+
+        myMoney += tempMoney;
+
+    }
+
+}
